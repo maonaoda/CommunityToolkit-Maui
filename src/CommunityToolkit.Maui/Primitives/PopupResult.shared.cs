2d6fd37fb400f47d53f20f8caf6656b0142e28db
maonaoda <maonaoda@gmail.com>
2025-12-16 12:47:03 +0900

    feat: apply IQueryAttributable from none-shell page

--- src/CommunityToolkit.Maui/Primitives/PopupResult.shared.cs
+++ src/CommunityToolkit.Maui/Primitives/PopupResult.shared.cs
@@ -7,13 +7,13 @@ namespace CommunityToolkit.Maui;
 /// Represents the result of a popup.
 /// </summary>
 /// <param name="WasDismissedByTappingOutsideOfPopup">True if Popup is closed by tapping outside popup</param>
-record PopupResult(bool WasDismissedByTappingOutsideOfPopup) : IPopupResult;
+public record PopupResult(bool WasDismissedByTappingOutsideOfPopup) : IPopupResult;
 
 /// <summary>
 /// Represents the result of a popup when T is a reference type.
 /// </summary>
 /// <typeparam name="T">Popup result type</typeparam>
-sealed record PopupResult<T> : PopupResult, IPopupResult<T>
+public record PopupResult<T> : PopupResult, IPopupResult<T>
 {
 	/// <summary>
 	/// Initialize <see cref="PopupResult{T}"/>.
@@ -27,9 +27,16 @@ sealed record PopupResult<T> : PopupResult, IPopupResult<T>
 			: (T?)(result ?? default(T)); // Non-Nullable types will only be cast to `T` when `result` is not null because .NET will throw an InvalidCastException when casting `null` to a value-type (e.g. bool, int). When result is null and T is a value-type, we will use the default type of T (e.g. default(int) == 0) and defer throwing the InvalidCastException until the developer retrieves the value of Result. 
 	}
 
+	/// <summary>
+	/// Popup result
+	/// </summary>
 	public T? Result => WasDismissedByTappingOutsideOfPopup && !typeof(T).IsNullable()
 		? throw new PopupResultException($"{nameof(Result)} is null, but {nameof(PopupResult)} type, {typeof(T)}, cannot be converted to null. Every time {nameof(WasDismissedByTappingOutsideOfPopup)} is {true}, {nameof(Result)} is always null. When using a non-nullable type (e.g. bool) be sure to first check if {nameof(WasDismissedByTappingOutsideOfPopup)} is {false} before getting the value of {nameof(Result)}")
 		: field;
 }
 
-sealed class PopupResultException(string message) : InvalidCastException(message);
+/// <summary>
+/// Exception thrown when PopupResult cannot cast Result to the specified type.
+/// </summary>
+/// <param name="message"></param>
+public class PopupResultException(string message) : InvalidCastException(message);
